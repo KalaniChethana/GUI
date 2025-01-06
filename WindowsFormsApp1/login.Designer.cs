9a8e8427a3e5275ee9e4c3b9095c6747ad6059e1
KalaniChethana <kalanichethananethmi2001@gmail.com>
2025-01-06 10:20:31 +0530

    new

---
+++ WindowsFormsApp1/login.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.UNameTb = new System.Windows.Forms.TextBox();
+            this.PasswordTb = new System.Windows.Forms.TextBox();
+            this.LogBtn = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.CrossBtn = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources._360_F_906684558_CxKt2b93efUg13ndU7vIGXGxFYDFQ6oM;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(799, 453);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(279, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 52);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Toy Shop";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(307, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(313, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // UNameTb
+            // 
+            this.UNameTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UNameTb.Location = new System.Drawing.Point(281, 136);
+            this.UNameTb.Name = "UNameTb";
+            this.UNameTb.Size = new System.Drawing.Size(214, 30);
+            this.UNameTb.TabIndex = 4;
+            // 
+            // PasswordTb
+            // 
+            this.PasswordTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordTb.Location = new System.Drawing.Point(281, 221);
+            this.PasswordTb.Name = "PasswordTb";
+            this.PasswordTb.Size = new System.Drawing.Size(214, 30);
+            this.PasswordTb.TabIndex = 5;
+            // 
+            // LogBtn
+            // 
+            this.LogBtn.BackColor = System.Drawing.Color.White;
+            this.LogBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.LogBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.LogBtn.Location = new System.Drawing.Point(313, 323);
+            this.LogBtn.Name = "LogBtn";
+            this.LogBtn.Size = new System.Drawing.Size(146, 38);
+            this.LogBtn.TabIndex = 6;
+            this.LogBtn.Text = "Login";
+            this.LogBtn.UseVisualStyleBackColor = false;
+            this.LogBtn.Click += new System.EventHandler(this.LogBtn_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.BackColor = System.Drawing.Color.White;
+            this.Reset.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Reset.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Reset.Location = new System.Drawing.Point(313, 367);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(146, 38);
+            this.Reset.TabIndex = 7;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = false;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // CrossBtn
+            // 
+            this.CrossBtn.BackColor = System.Drawing.Color.White;
+            this.CrossBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.CrossBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CrossBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.CrossBtn.Location = new System.Drawing.Point(764, -2);
+            this.CrossBtn.Name = "CrossBtn";
+            this.CrossBtn.Size = new System.Drawing.Size(34, 32);
+            this.CrossBtn.TabIndex = 8;
+            this.CrossBtn.Text = "X";
+            this.CrossBtn.UseVisualStyleBackColor = false;
+            this.CrossBtn.Click += new System.EventHandler(this.CrossBtn_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(313, 257);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(151, 24);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Show Password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.CrossBtn);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.LogBtn);
+            this.Controls.Add(this.PasswordTb);
+            this.Controls.Add(this.UNameTb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "login";
+            this.Text = "login";
+            this.Load += new System.EventHandler(this.login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox UNameTb;
+        private System.Windows.Forms.TextBox PasswordTb;
+        private System.Windows.Forms.Button LogBtn;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button CrossBtn;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
